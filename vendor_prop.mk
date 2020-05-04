dc9eb32d79148654ab7034e1935dc20885a5f469
Bikram Mukherjee <kitkatmukherjee2015@gmail.com>
2020-05-04 21:39:44 +0600

    santoni: Fix rendering lags for screen recoarder
    Also organize a bit
    Fix all lags in ui and when using screen recorder or/and cpu info

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,15 +103,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=0 \
-debug.sf.enable_gl_backpressure=1 \
-debug.egl.hw=0 \
 persist.hwc.mdpcomp.enable=true \
 debug.mdpcomp.logs=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
-debug.enable.sglscale=1 \
 sdm.debug.disable_skip_validate=1 \
-debug.sf.recomputecrop=0 \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
 ro.sys.fw.dex2oat_thread_count=8 \
@@ -282,9 +278,9 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
 ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000
 
@@ -292,7 +288,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.early_phase_offset_ns=1500000 \
 debug.sf.early_app_phase_offset_ns=1500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.latch_unsignaled=1 \
+debug.sf.disable_backpressure=1 \
+debug.sf.enable_gl_backpressure=1
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
